0f8430d5044b6dbf7c3965f67d084b434aea83c5
patriciochavez <patricio_chavez@hotmail.com>
2017-06-19 15:30:44 -0300

    new UI

--- app/src/main/java/net/chavezp/publishmqtt/MainActivity.java
+++ app/src/main/java/net/chavezp/publishmqtt/MainActivity.java
@@ -1,13 +1,15 @@
 package net.chavezp.publishmqtt;
 
-import net.chavezp.publishmqtt.Suscripcion;
-
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
@@ -15,21 +17,24 @@ import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.io.UnsupportedEncodingException;
 
-import static android.R.id.message;
-
 public class MainActivity extends AppCompatActivity {
 
-    private String clientId;
+    final String STRING_CONN = "tcp://10.105.231.153:1883";
     final String TAG = "casa";
-    //private Boolean iamconnected = false;
+
+    private String clientId;
+    private Boolean iamconnected = false;
     private Button buttonConnect;
     private Button buttonLuzPorton;
+
     public static TextView textviewEstado;
+    public MqttAndroidClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,49 +44,138 @@ public class MainActivity extends AppCompatActivity {
         buttonConnect = (Button) findViewById(R.id.button_connect);
         buttonConnect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
-                //Log.d(TAG,"Boton presionado");
-                connect(v);
+                disconnect();
             }
         });
 
         textviewEstado = (TextView) findViewById(R.id.textview_estado);
+
         buttonLuzPorton = (Button) findViewById(R.id.button_luz_porton);
         buttonLuzPorton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
-                sendMessage(v);
-                getStatus(v);
-                //Log.d(TAG,"Boton presionado");
+                publish("casa/luz/porton", "cambiar");
+                //getStatus(v);
             }
         });
-        buttonLuzPorton.setEnabled(false);
+
+        connect();
     }
 
-    public MqttAndroidClient client;
+    @Override
+    protected void onStop() {
+        // call the superclass method first
+        super.onStop();
+        //Disconnect
+        disconnect();
+    }
 
-    public void connect(View v) {
+    public void connect() {
+        if (iamconnected) return;
+        //getStatus("casa/estado", "todo");
+
         clientId = MqttClient.generateClientId();
-        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://IP:PORT", clientId);
+        client = new MqttAndroidClient(this.getApplicationContext(), STRING_CONN, clientId);
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        options.setUserName("mi_usuario");
+        options.setPassword("mi_clave".toCharArray());
 
         {
             try {
-                IMqttToken token = client.connect();
+                IMqttToken token = client.connect(options);
+                token.setActionCallback(new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        Log.d(TAG, "onSuccess");
+                        iamconnected = true;
+                        refreshUI();
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Log.d(TAG, "onFailure");
+                        iamconnected = false;
+                        refreshUI();
+                    }
+                });
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void refreshUI() {
+        if (iamconnected) {
+            //Connect button
+            buttonConnect.setText("Desconectar");
+            buttonConnect.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    disconnect();
+                }
+            });
+
+            //Light button
+            buttonLuzPorton.setEnabled(true);
+        }
+        //Not connected
+        else {
+            //Connect Button
+            buttonConnect.setText("Conectar");
+            buttonConnect.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    connect();
+                }
+            });
+
+            //Light button
+            buttonLuzPorton.setEnabled(false);
+
+        }
+        //Temperature
+
+        //Ligth
+
+        //Buzzer
+    }
+
+
+    public void connect(View v) {
+        if (iamconnected) return;
+
+        clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), STRING_CONN, clientId);
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        options.setUserName("mi_usuario");
+        options.setPassword("mi_clave".toCharArray());
+
+        {
+            try {
+                IMqttToken token = client.connect(options);
                 token.setActionCallback(new IMqttActionListener() {
                     @Override
                     public void onSuccess(IMqttToken asyncActionToken) {
                         // We are connected
                         Log.d(TAG, "onSuccess");
-                        //iamconnected = true;
-                        buttonConnect.setText("Desconectar");
+                        iamconnected = true;
+                        /*buttonConnect.setText("Desconectar");
                         buttonConnect.setOnClickListener(new View.OnClickListener() {
                             public void onClick(View v) {
                                 // Do something in response to button click
                                 disconnect(v);
                                 //Log.d(TAG,"Boton presionado");
                             }
-                        });
-                        buttonLuzPorton.setEnabled(true);
+                        });*/
+
+                        /*buttonConnect.setText("Desconectar");
+                        buttonConnect.setOnClickListener(new View.OnClickListener() {
+                            public void onClick(View v) {
+                                // Do something in response to button click
+                                disconnect(v);
+                                //Log.d(TAG,"Boton presionado");
+                            }
+                        });*/
+                        //buttonLuzPorton.setEnabled(true);
 
                         String topic = "casa/#";
                         int qos = 0;
@@ -113,8 +207,8 @@ public class MainActivity extends AppCompatActivity {
                         // Something went wrong e.g. connection timeout or firewall problems
                         Log.d(TAG, "onFailure");
                         //iamconnected = false;
-                        buttonConnect.setText("Conectar");
-                        buttonLuzPorton.setEnabled(false);
+                        //buttonConnect.setText("Conectar");
+                        //buttonLuzPorton.setEnabled(false);
                     }
                 });
             } catch (MqttException e) {
@@ -123,30 +217,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void disconnect(View v) {
+    public void disconnect() {
         try {
             IMqttToken disconToken = client.disconnect();
             disconToken.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    // we are now successfully disconnected
-                    //iamconnected = false;
-                    buttonConnect.setText("Conectar");
-                    buttonConnect.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v) {
-                            // Do something in response to button click
-                            connect(v);
-                            //Log.d(TAG,"Boton presionado");
-                        }
-                    });
-                    buttonLuzPorton.setEnabled(false);
+                    iamconnected = false;
+                    refreshUI();
                 }
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken,
                                       Throwable exception) {
-                    // something went wrong, but probably we are disconnected anyway
-                    buttonLuzPorton.setEnabled(false);
+                    refreshUI();
                 }
             });
         } catch (MqttException e) {
@@ -155,14 +239,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * Called when the user touches the button
-     */
     public void sendMessage(View view) {
-        // Do something in response to button click
-        //if (iamconnected){
-        //Log.d(TAG, "I am connected");
-        //Log.d(TAG, "Sending new message");
 
         String topic = "casa/luz/porton";
         String payload = "a";
@@ -178,14 +255,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void getStatus(View view) {
-        // Do something in response to button click
-        //if (iamconnected){
-        //Log.d(TAG, "I am connected");
-        //Log.d(TAG, "Sending new message");
+    public void publish(String topic, String payload) {
 
-        String topic = "casa/estado";
-        String payload = "a";
         byte[] encodedPayload = new byte[0];
         {
             try {
@@ -196,6 +267,30 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-        //}
+    }
+
+    public void getStatus(String topic, String payload) {
+
+        if (!iamconnected) {
+            Context context = getApplicationContext();
+            CharSequence text = "No conectado!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
+            return;
+        } else {
+        byte[] encodedPayload = new byte[0];
+        {
+            try {
+                encodedPayload = payload.getBytes("UTF-8");
+                MqttMessage message = new MqttMessage(encodedPayload);
+                client.publish(topic, message);
+            } catch (UnsupportedEncodingException | MqttException e) {
+                e.printStackTrace();
+            }
+            }
+        }
     }
 }
